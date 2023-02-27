8adba3846c8166a5447f1204f57228413b67b60c
Fadlullah04 <fadlumuhd570@gmail.com>
2023-02-27 08:23:29 -0800

    Print a string!

---
+++ c/recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+
+/*_puts_recursion - prints a string
+ * @char *: string to be printed
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else 
+		_putchar('\n');
+}
---
+++ c/recursion/main.h
@@ -0,0 +1,5 @@
+#ifndef _FILE_
+#define _FILE_
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif
